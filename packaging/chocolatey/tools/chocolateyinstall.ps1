d2476d85ce5bf121353af29f8cee5789a73fc39d
Empirica System <soulentheo@gmail.com>
2026-08-12 17:53:23 +0200

    chore: automated release 1.13.11
    - Updated all distribution channels
    - SHA256: eb2d82bd202336dbf654816b2812f57e9e0cb1e595358ae3712b05d9f7b1abdf

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 $packageName = 'empirica'
 $packageVersion = '1.13.11'
 $url = "https://files.pythonhosted.org/packages/source/e/empirica/empirica-$packageVersion.tar.gz"
-$checksum = 'c6f6a9671f03e48e5f59053d4d3617074dcbf934e2eabda8f05856ee667c3d7d'  # TODO: Update sha256 after PyPI publish
+$checksum = 'eb2d82bd202336dbf654816b2812f57e9e0cb1e595358ae3712b05d9f7b1abdf'  # TODO: Update sha256 after PyPI publish
 $checksumType = 'sha256'
 
 Write-Host "Installing Empirica $packageVersion..." -ForegroundColor Cyan
--- packaging/homebrew/empirica.rb
+++ packaging/homebrew/empirica.rb
@@ -13,8 +13,8 @@ class Empirica < Formula
 
   desc "Epistemic self-assessment framework for AI agents"
   homepage "https://github.com/EmpiricaAI/empirica"
-  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.13.10.tar.gz"
-  sha256 "c6f6a9671f03e48e5f59053d4d3617074dcbf934e2eabda8f05856ee667c3d7d"
+  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.13.11.tar.gz"
+  sha256 "eb2d82bd202336dbf654816b2812f57e9e0cb1e595358ae3712b05d9f7b1abdf"
   license "MIT"
 
   depends_on "python@3.11"
@@ -49,7 +49,7 @@ class Empirica < Formula
 
   test do
     # Test that the CLI works
-    assert_match "1.13.10", shell_output("#{bin}/empirica --version")
+    assert_match "1.13.11", shell_output("#{bin}/empirica --version")
 
     # Test that key commands exist
     system "#{bin}/empirica", "session-create", "--help"
